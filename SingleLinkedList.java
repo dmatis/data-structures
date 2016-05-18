c2d0732bcbcc5ba8e27f2a074e68977efa191a35
dmatis <darren.matis@gmail.com>
2016-05-18 14:11:36 -0700

    Made Node a private class

--- SingleLinkedList.java
+++ SingleLinkedList.java
@@ -67,4 +67,30 @@ public class SingleLinkedList {
         myLL.printList();
         System.out.println("After deletion, size is: " + myLL.getSize());
     }
+
+    private class Node {
+        Node next;
+        Object data;
+
+        public Node(Object data) {
+            this(data,null);
+        }
+
+        public Node(Object data, Node next) {
+            this.next = next;
+            this.data = data;
+        }
+
+        public Object getData() {
+            return this.data;
+        }
+
+        public Node getNext() {
+            return this.next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
 }
